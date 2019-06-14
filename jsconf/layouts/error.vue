904bc29772ce31045d7b1fd6265ae96b33208339
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-14 14:57:59 +0800

    created error.vue in layout folder

--- jsconf/layouts/error.vue
+++ jsconf/layouts/error.vue
@@ -1,44 +1,20 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <div>
+    <h1 v-if="error.statusCode === 404">Page not found (using layouts/error.vue)</h1>
+    <h1 v-else>An error occurred</h1>
+    <p>Route path is {{ $route.path }}</p>
+    <p>Route query is {{ $route.query }}</p>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
 </template>
 
 <script>
 export default {
-  layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null
-    }
-  },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
-  },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
-    }
+  props: ['error'],
+  layout: 'default',
+  mounted() {
+    alert('You can do a redirect also when mounted')
+    // this.$router.push('/dynamic/2')
   }
 }
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
